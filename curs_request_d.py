0e78c38fe108bf6e771b17ca7e024227315eae12
isva1960 <isva1960@gmail.com>
2024-06-03 15:57:44 +0300

    Изменение в коде. Функции по основным валютам выведены из головной.

--- curs_request_d.py
+++ curs_request_d.py
@@ -2,15 +2,10 @@ import requests
 import datetime
 
 
-def request_curs(pdate):  # Функция по дате получает курсы валют и выводит на экран.
+def request_curs(pdate, date_curs):  # Функция по дате получает курсы валют и выводит на экран.
     try:
         # Делаем запрос. Параметры periodicity=0 - ежедневный курс, ondate=pdate (дата курсов, вводится в программе)
         # res - ответ
-        print('Основные валюты')
-        request_curs_val(pdate,'USD')
-        request_curs_val(pdate,'EUR')
-        request_curs_val(pdate,'RUB')
-        print('Прочие валюты')
         res = requests.get("https://api.nbrb.by/exrates/rates", params={"periodicity": 0, "ondate": pdate})
         date = res.json()
         if date:  # Данные есть
@@ -32,30 +27,48 @@ def request_curs(pdate):  # Функция по дате получает кур
                 cdate = datetime.datetime.strptime(curs['Date'], '%Y-%m-%dT%H:%M:%S').strftime("%Y.%m.%d")
                 # Выводим результат, типа
                 # Код валюты: 459, Дата: 2024.06.01, Код: KZT, Курс: 7.2022 за 1000 Тенге
-                if  curs['Cur_Abbreviation'] != 'USD' and curs['Cur_Abbreviation'] != 'EUR' and curs['Cur_Abbreviation'] != 'RUB':
+                if curs['Cur_Abbreviation'] != 'USD' and curs['Cur_Abbreviation'] != 'EUR' and curs[
+                    'Cur_Abbreviation'] != 'RUB':
                     print(
                         f"Код валюты: {curs['Cur_ID']}, Дата: {cdate}, Код: {curs['Cur_Abbreviation']}, Курс: {curs['Cur_OfficialRate']} за {curs['Cur_Scale']} {curs['Cur_Name']}")
         else:  # Данных нет
-            print("Нет данных!")
+            print(f"Нет данных за {date_curs}!")
     except Exception as e:
         print("Ошибка :", e)
 
-def request_curs_val(pdate,val):
-    res = requests.get(f"https://api.nbrb.by/exrates/rates/{val}",
-                       params={"periodicity": 0, "ondate": pdate, "parammode": 2})
-    date = res.json()
-    if date:  # Данные есть
-        cdate = datetime.datetime.strptime(date['Date'], '%Y-%m-%dT%H:%M:%S').strftime("%Y.%m.%d")
-        print(
-            f"Код валюты: {date['Cur_ID']}, Дата: {cdate}, Код: {date['Cur_Abbreviation']}, Курс: {date['Cur_OfficialRate']} за {date['Cur_Scale']} {date['Cur_Name']}")
+
+def request_curs_val(pdate, date_curs, val):
+    try:
+        res = requests.get(f"https://api.nbrb.by/exrates/rates/{val}",
+                           params={"periodicity": 0, "ondate": pdate, "parammode": 2})
+    except Exception as e:
+        print("Ошибка :", e)
+    else:
+        date = res.json()
+        if 'status' in date:
+            if date['status'] == 404:
+                date = ""
+        if date:  # Данные есть
+            cdate = datetime.datetime.strptime(date['Date'], '%Y-%m-%dT%H:%M:%S').strftime("%Y.%m.%d")
+            print(
+                f"Код валюты: {date['Cur_ID']}, Дата: {cdate}, Код: {date['Cur_Abbreviation']}, Курс: {date['Cur_OfficialRate']} за {date['Cur_Scale']} {date['Cur_Name']}")
+        else:  # Данных нет
+            print(f"Нет данных по валюте {val} за {date_curs}!")
+
 
 date_curs = input("Введите дату в виде ДД.ММ.ГГГГ: ")
 try:
     # Преобразовываем дату ДД.ММ.ГГГГ в ГГГГ-ММ-ДД
     formatted_date = datetime.datetime.strptime(date_curs, '%d.%m.%Y').strftime('%Y-%m-%d')
     # если дата задана верно, выполняем запрос
-    request_curs(formatted_date)
 except:
     # Если дата задана неверно, выводим сообщение об ошибке
     print("Дата задана неверно!")
+else:
+    print('Основные валюты')
+    request_curs_val(formatted_date, date_curs, 'USD')
+    request_curs_val(formatted_date, date_curs, 'EUR')
+    request_curs_val(formatted_date, date_curs, 'RUB')
+    print('Прочие валюты')
+    request_curs(formatted_date, date_curs)
 input("Нажмите Enter")
